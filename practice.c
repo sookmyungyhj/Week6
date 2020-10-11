0c86f85e4b9f71790ff8b523f75d0795be3f8165
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-12 01:24:08 +0900

    practice 3-1(1)

--- practice.c
+++ practice.c
@@ -3,9 +3,9 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+void square(int a)
 {
-	a = a * a;
+	return (a*a);
 }
 
 
