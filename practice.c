7140ae8164bd57717b3bfc6b4e121e1f25aae3a8
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-12 01:16:49 +0900

    practice2

--- practice.c
+++ practice.c
@@ -2,18 +2,33 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void print_star()
+int sumTwo(int a, int b)
 {
-	int i;
-	for(i=0;i<10;i++)
-		printf("*");
+	int sumTwo_output;
+	sumTwo_output = a + b;
+	return sumTwo_output;
+}
+
+int square(int n)
+{
+	return (n*n);
+}
+
+int get_max(int x, int y)
+{
+	if (x > y) return x;
+	else return y;
 }
 
 
 int main(void) {
-	print_star();
-	print_star();
-	print_star();
 	
+	int sum, square_output, max;
+	
+	sum = sumTwo(1,2);
+	square_output = square(3);
+	max = get_max(4,5);
+	
+	printf("%d, %d, %d",sum,square_output,max);
 	return 0;
 }
