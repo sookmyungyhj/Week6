1ee91e5ed2f588d9d38587a96a51d6032ce4ae28
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-12 01:01:15 +0900

    practice1-1
    모듈화

--- practice.c
+++ practice.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void print_star()
+{
+	int i;
+	for(i=0;i<10;i++)
+		printf("*");
+}
+
 
 int main(void) {
-	int i;
+	print_star();
+	print_star();
+	print_star();
 	
-	for(i=0;i<10;i++)
-		printf("*");
-	for(i=0;i<10;i++)
-		printf("*");
-	for(i=0;i<10;i++)
-		printf("*");
-		
 	return 0;
 }
