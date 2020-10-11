bc72a23948333c61c44dcd1ac6bc48023d916a41
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-10-12 01:19:40 +0900

    practice3-1

--- practice.c
+++ practice.c
@@ -2,33 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int sumTwo(int a, int b)
-{
-	int sumTwo_output;
-	sumTwo_output = a + b;
-	return sumTwo_output;
-}
 
-int square(int n)
+int square(int a)
 {
-	return (n*n);
-}
-
-int get_max(int x, int y)
-{
-	if (x > y) return x;
-	else return y;
+	a = a * a;
 }
 
 
-int main(void) {
-	
-	int sum, square_output, max;
-	
-	sum = sumTwo(1,2);
-	square_output = square(3);
-	max = get_max(4,5);
-	
-	printf("%d, %d, %d",sum,square_output,max);
+int main()
+{
+	int a = 2;
+	square(a);	
+	printf("a=%i\n",a);
 	return 0;
 }
